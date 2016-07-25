008492ca04c213e5db1ff0ccc7179157be511865
Riley Edmunds <rileyedmunds@berkeley.edu>
2016-07-25 17:47:36 -0500

    parsing and cleaning for one sample

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pylab
 import requests
+import numpy as np
 from bs4 import BeautifulSoup as bs4
 
 #define up page to scrape:
@@ -19,8 +20,44 @@ html = bs4(get.text, 'html.parser')
 apts = html.find_all('p', attrs={'class': 'row'})
 # print(len(apts))
 #note: bs4 prettify prints cleanly as XML with each tag on its own line:
-one_apt = apts[10]
+one_apt = apts[15]
 # print(one_apt.prettify())
 # size = one_apt.findAll(attrs={'class': 'housing'})[0].text
 size = one_apt.findAll(attrs={'class' : 'housing'})[0].text
-print(size)
+
+#clean and parse size and bedrooms, dealing with case where one is missing
+def extract_size_and_brs(size):
+    reduced = size.strip('/- ').split(' - ')
+    if len(reduced) == 2:
+        n_brs = reduced[0].replace('br', '')
+        this_size = reduced[1].replace('ft2', '')
+    elif 'br' in reduced[0]:
+        #there's no footage
+        n_brs = reduced[0].replace('br', '')
+        this_size = np.nan
+    elif 'ft2' in reduced[0]:
+        #there's no bedrooms
+        n_brs = np.nan
+        this_size = reduced[0].replace('ft2','')
+    return float(this_size), float(n_brs)
+
+#clean parse time as datetime
+def extract_time(input):
+    time = input.find('time')['datetime']
+    datetime = pd.to_datetime(time)
+    return datetime
+
+#clean and parse price as float
+def extract_price(input):
+    price = input.find('span', {'class': 'price'})
+    price_clean = float(price.text.strip('$'))
+    return price_clean
+
+this_size,this_n_brs = extract_size_and_brs(size)
+this_time = extract_time(one_apt)
+this_price = extract_price(one_apt)
+
+print(this_size)
+print(this_n_brs)
+print(this_time)
+print(this_price)
