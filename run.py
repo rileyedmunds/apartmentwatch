d0fca0ed1e3a7123dd42b2ea1953fc88f927ec14
Riley Edmunds <rileyedmunds@berkeley.edu>
2016-07-26 17:33:43 -0500

    working v1.4 (refactor)

---
+++ run.py
@@ -0,0 +1,81 @@
+#AUTORUN: continuous monitoring of craigslist messages
+
+import requests
+import numpy as np
+from bs4 import BeautifulSoup as bs4
+import string
+import time
+from twilio.rest import TwilioRestClient
+import scraper
+
+# ----------------------messaging----------------------------
+
+#sending message with specified body
+def send_message(message_content):
+    # setting up twilio (importing keys from file for public repo security)
+    file = open("/Users/rileyedmunds/credentials/twilio", 'r')
+    ACCOUNT_SID = file.readline().split()[2]
+    AUTH_TOKEN = file.readline().split()[2]
+    TWILIO_NUMBER = file.readline().split()[2]
+    MY_NUMBER = file.readline().split()[2]
+
+    #sending message
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+    client.messages.create(
+        to= MY_NUMBER,
+        from_= TWILIO_NUMBER,
+        body= message_content
+    )
+
+# -------------------------------------------------------
+
+# defining the url and a query we want to post
+base_url = 'https://chicago.craigslist.org/'
+url = base_url + 'search/wcl/apa?s=5&bedrooms=1'
+
+link_list = []  # storing the data here
+link_list_send = []  # list of links to be sent
+send_list = []  # what will actually be sent
+
+#avoid all output from the first run:
+first_run = True
+
+# note: too many queries => IP banned
+while True:
+    resp = requests.get(url)
+    txt = bs4(resp.text, 'html.parser')
+    apts = txt.findAll(attrs={'class': "row"})
+
+    # We're just going to pull the title and link
+    for apt in apts:
+        title = apt.find_all('a', attrs={'class': 'hdrlnk'})[0]
+        use_chars = string.ascii_letters + \
+                    ''.join([str(i) for i in range(10)]) + \
+                    ' /\.'
+        name = ''.join([i for i in title.text if i in use_chars])
+        link = title.attrs['href']
+        if link not in link_list and link not in link_list_send and not first_run:
+            print('Found new listing')
+            link_list_send.append(link)
+            send_list.append(name + '  -  ' + base_url + link)
+            print(name + base_url + link)
+
+    # send message, then flush cache
+    if len(link_list_send) > 0:
+        print('Sending text!')
+        msg = '\n'.join(send_list)
+        #let's not break twilio or kill my trial here :)
+        if (len(msg) < 200):
+            send_message(msg)
+
+        #flush cache
+        link_list += link_list_send
+        link_list_send = []
+        send_list = []
+
+    # sleeping so craigslist doesn't ban my IP
+    sleep_duration = np.random.randint(5, 10) #60 to 120 seconds
+    time.sleep(sleep_duration)
+
+    if (first_run):
+        first_run = False
---
+++ testscraper.py
@@ -0,0 +1,82 @@
+#TESTSCRAPER: [predecessor to scraper] pulls one listing and its information
+
+import pandas as pd
+import pylab
+import requests
+import numpy as np
+from bs4 import BeautifulSoup as bs4
+
+#define up page to scrape:
+url_short = 'https://chicago.craigslist.org/search/wcl/apa'
+parameters = dict(bedrooms=1, is_furnished=1)
+get = requests.get(url_short, params=parameters)
+
+#confirm requests parsed the url correctly
+#print(get.url) # print(get.text[:500])
+
+#parse response as HTML using bs4:
+html = bs4(get.text, 'html.parser')
+# print(html.prettify()[:1000])
+
+#beautiful soup can find all instances of a container:
+apts = html.find_all('p', attrs={'class': 'row'})
+# print(len(apts))
+#note: bs4 prettify prints cleanly as XML with each tag on its own line:
+one_apt = apts[15]
+# print(one_apt.prettify())
+
+#clean and parse size and bedrooms, dealing with case where one is missing
+def extract_size_and_brs(input):
+    size = input.findAll(attrs={'class': 'housing'})[0].text
+    reduced = size.strip('/- ').split(' - ')
+    if len(reduced) == 2:
+        n_brs = reduced[0].replace('br', '')
+        this_size = reduced[1].replace('ft2', '')
+    elif 'br' in reduced[0]:
+        #there's no footage
+        n_brs = reduced[0].replace('br', '')
+        this_size = np.nan
+    elif 'ft2' in reduced[0]:
+        #there's no bedrooms
+        n_brs = np.nan
+        this_size = reduced[0].replace('ft2','')
+    return float(this_size), float(n_brs)
+
+#clean parse time as datetime
+def extract_time(input):
+    time = input.find('time')['datetime']
+    datetime = pd.to_datetime(time)
+    return datetime
+
+#clean and parse price as float
+def extract_price(input):
+    price = input.find('span', {'class': 'price'})
+    price_clean = float(price.text.strip('$'))
+    return price_clean
+
+#clean and parse price as float
+def extract_title(input):
+    title = input.find('a', {'class': 'hdrlnk'})
+    title_text = title.text
+    return title_text
+
+
+this_size,this_n_brs = extract_size_and_brs(one_apt)
+this_time = extract_time(one_apt)
+this_price = extract_price(one_apt)
+this_title = extract_title(one_apt)
+
+for i in range(4):
+    curr_apt = apts[i]
+    curr_size, curr_n_brs = extract_size_and_brs(curr_apt)
+    curr_time = extract_time(curr_apt)
+    curr_price = extract_price(curr_apt)
+    curr_title = extract_title(curr_apt)
+
+    print(curr_size)
+    print(curr_n_brs)
+    print(curr_time)
+    print(curr_price)
+    print(curr_title)
+    print('\n')
+
