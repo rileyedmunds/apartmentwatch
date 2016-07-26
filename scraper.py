8167cbf32b48c7d76f1430f3a708e2aa646369c5
Riley Edmunds <rileyedmunds@berkeley.edu>
2016-07-26 17:33:09 -0500

    working v1.4 (refactor)

--- scraper.py
+++ scraper.py
@@ -1,80 +1,141 @@
+#SCRAPER: pull craiglist apartment listings into CSV file. Allows for plotting.
+
+
 import pandas as pd
 import pylab
 import requests
 import numpy as np
 from bs4 import BeautifulSoup as bs4
+import string
+import time
+import matplotlib as plt
 
-#define up page to scrape:
-url_short = 'https://chicago.craigslist.org/search/wcl/apa'
-parameters = dict(bedrooms=1, is_furnished=1)
-get = requests.get(url_short, params=parameters)
 
-#confirm requests parsed the url correctly
-#print(get.url) # print(get.text[:500])
+#-------------vital functions-------------
 
-#parse response as HTML using bs4:
-html = bs4(get.text, 'html.parser')
-# print(html.prettify()[:1000])
-
-#beautiful soup can find all instances of a container:
-apts = html.find_all('p', attrs={'class': 'row'})
-# print(len(apts))
-#note: bs4 prettify prints cleanly as XML with each tag on its own line:
-one_apt = apts[15]
-# print(one_apt.prettify())
-
-#clean and parse size and bedrooms, dealing with case where one is missing
-def extract_size_and_brs(input):
-    size = input.findAll(attrs={'class': 'housing'})[0].text
-    reduced = size.strip('/- ').split(' - ')
-    if len(reduced) == 2:
-        n_brs = reduced[0].replace('br', '')
-        this_size = reduced[1].replace('ft2', '')
-    elif 'br' in reduced[0]:
-        #there's no footage
-        n_brs = reduced[0].replace('br', '')
+# cleaning and parsing size and bedrooms, dealing with case where one is missing
+def extract_size_and_brs(size):
+    split = size.strip('/- ').split(' - ')
+    if len(split) == 2:
+        n_brs = split[0].replace('br', '')
+        this_size = split[1].replace('ft2', '')
+    elif 'br' in split[0]:
+        # It's the n_bedrooms
+        n_brs = split[0].replace('br', '')
         this_size = np.nan
-    elif 'ft2' in reduced[0]:
-        #there's no bedrooms
+    elif 'ft2' in split[0]:
+        # It's the size
+        this_size = split[0].replace('ft2', '')
         n_brs = np.nan
-        this_size = reduced[0].replace('ft2','')
     return float(this_size), float(n_brs)
 
-#clean parse time as datetime
+#cleaning and parsing time as datetime
 def extract_time(input):
     time = input.find('time')['datetime']
     datetime = pd.to_datetime(time)
     return datetime
 
-#clean and parse price as float
+#cleaning and parsing price as float
 def extract_price(input):
     price = input.find('span', {'class': 'price'})
     price_clean = float(price.text.strip('$'))
     return price_clean
 
-#clean and parse price as float
+#dealing with errors in batch price extraction:
+def find_prices(inputs):
+    prices = []
+    for a in inputs:
+        price = a.find('span', {'class': 'price'})
+        if price is not None:
+            price = float(price.text.strip('$'))
+        else:
+            price = np.nan
+        prices.append(price)
+    return prices
+
+#cleaning and parsing title
 def extract_title(input):
     title = input.find('a', {'class': 'hdrlnk'})
     title_text = title.text
     return title_text
 
+#-----------Non-vital functions----------
 
-this_size,this_n_brs = extract_size_and_brs(one_apt)
-this_time = extract_time(one_apt)
-this_price = extract_price(one_apt)
-this_title = extract_title(one_apt)
+#plots histogram of prices.
+    #NOTE: will only work with matplotlib inline (IPython notebook)
+def draw_hist(data):
+    e = results.hist('price', bins=np.arange(0, 10000, 100))[0, 0]
+    ax.set_title('price vs count', fontsize = 30)
+    ax.set_xlabel('Price', fontsize=15)
+    ax.set_ylabel('Count', fontsize=15)
 
-for i in range(4):
-    curr_apt = apts[i]
-    curr_size, curr_n_brs = extract_size_and_brs(curr_apt)
-    curr_time = extract_time(curr_apt)
-    curr_price = extract_price(curr_apt)
-    curr_title = extract_title(curr_apt)
+#saves file as CSV:
+def save_to_file(data):
+    # cleaning extraneous characters out:
+    charset = string.ascii_letters + \
+                ''.join([str(i) for i in range(10)]) + \
+                ' /\.'
+    results['title'] = results['title'].apply(
+        lambda a: ''.join([i for i in a if i in charset]))
 
-    print(curr_size)
-    print(curr_n_brs)
-    print(curr_time)
-    print(curr_price)
-    print(curr_title)
-    print('\n')
+    results.to_csv('data/craigslist_results.csv')
+
+
+# def text_updates:
+#----------------------------------------
+
+
+#storing outcomes here
+results = []
+
+#note to not request too often => IP ban from Craigslist
+search_indices = np.arange(5, 300, 300)
+for i in search_indices:
+    url = 'https://chicago.craigslist.org/search/wcl/apa'
+    resp = requests.get(url, params={'bedrooms': 1, 's': i})
+    txt = bs4(resp.text, 'html.parser')
+    print(resp.url)
+    apts = txt.findAll(attrs={'class': "row"})
+    print(apts)
+
+    # finding the size of all listings
+    size_text = [rw.findAll(attrs={'class': 'housing'})[0].text
+                 for rw in apts]
+    print(size_text)
+
+    sizes_brs = [extract_size_and_brs(stxt) for stxt in size_text]
+    print(sizes_brs)
+    sizes, n_brs = zip(*sizes_brs)
+
+    # finding the title & link
+    title = [rw.find('a', attrs={'class': 'hdrlnk'}).text
+             for rw in apts]
+    links = [rw.find('a', attrs={'class': 'hdrlnk'})['href']
+             for rw in apts]
+
+    # finding the time
+    time = [pd.to_datetime(rw.find('time')['datetime']) for rw in apts]
+    price = find_prices(apts)
+
+    # populating the dataframe from a dictionary
+    data = {'time' : time, 'price': price,
+            'size' : list(sizes), 'brs': n_brs,
+            'title': title, 'links': links}
+    df = pd.DataFrame(data)
+    df = df.set_index('time')
+
+    #adding the newly created dataframe to the results
+    results.append(df)
+    print(df.head())
+
+# concatenating all the results
+results = pd.concat(results, axis=0)
+
+#fixing types of the numerical columns:
+results[['price', 'size', 'brs']] = results[['price', 'size', 'brs']].convert_objects(convert_numeric=True)
+save_to_file(results)
+
+
+
+
 
