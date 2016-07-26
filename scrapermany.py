c32db78b8117d48cd129cdd1a88a941b7d21a2f3
Riley Edmunds <rileyedmunds@berkeley.edu>
2016-07-26 15:47:27 -0500

    working v1.0

--- scrapermany.py
+++ scrapermany.py
@@ -4,7 +4,7 @@ import requests
 import numpy as np
 from bs4 import BeautifulSoup as bs4
 
-# clean and parse size and bedrooms, dealing with case where one is missing
+# cleaning and parsing size and bedrooms, dealing with case where one is missing
 def extract_size_and_brs(size):
     split = size.strip('/- ').split(' - ')
     if len(split) == 2:
@@ -20,19 +20,19 @@ def extract_size_and_brs(size):
         n_brs = np.nan
     return float(this_size), float(n_brs)
 
-#clean parse time as datetime
+#cleaning and parsing time as datetime
 def extract_time(input):
     time = input.find('time')['datetime']
     datetime = pd.to_datetime(time)
     return datetime
 
-#clean and parse price as float
+#cleaning and parsing price as float
 def extract_price(input):
     price = input.find('span', {'class': 'price'})
     price_clean = float(price.text.strip('$'))
     return price_clean
 
-#deal with errors in batch price extraction:
+#dealing with errors in batch price extraction:
 def find_prices(inputs):
     prices = []
     for a in inputs:
@@ -44,17 +44,17 @@ def find_prices(inputs):
         prices.append(price)
     return prices
 
-#clean and parse price as float
+#cleaning and parsing title
 def extract_title(input):
     title = input.find('a', {'class': 'hdrlnk'})
     title_text = title.text
     return title_text
 
 
-# store outcomes in results
+#storing outcomes here
 results = []
 
-#Note to not request too often => IP ban from Craigslist
+#note to not request too often => IP ban from Craigslist
 search_indices = np.arange(5, 300, 300)
 for i in search_indices:
     url = 'https://chicago.craigslist.org/search/wcl/apa'
@@ -63,22 +63,22 @@ for i in search_indices:
     apts = txt.findAll(attrs={'class': "row"})
     print(apts)
 
-    # Find the size of all listings
+    # finding the size of all listings
     size_text = [rw.findAll(attrs={'class': 'housing'})[0].text
                  for rw in apts]
     print(size_text)
 
     sizes_brs = [extract_size_and_brs(stxt) for stxt in size_text]
     print(sizes_brs)
-    sizes, n_brs = zip(*sizes_brs)  # This unzips into 2 vectors
+    sizes, n_brs = zip(*sizes_brs)
 
-    # Find the title and link
+    # finding the title & link
     title = [rw.find('a', attrs={'class': 'hdrlnk'}).text
              for rw in apts]
     links = [rw.find('a', attrs={'class': 'hdrlnk'})['href']
              for rw in apts]
 
-    # Find the time
+    # finding the time
     time = [pd.to_datetime(rw.find('time')['datetime']) for rw in apts]
     price = find_prices(apts)
 
@@ -89,13 +89,13 @@ for i in search_indices:
     df = pd.DataFrame(data)
     df = df.set_index('time')
 
-    #add the newly created dataframe to the results
+    #adding the newly created dataframe to the results
     results.append(df)
     print(df.head())
 
-# Finally, concatenate all the results
+# concatenating all the results
 results = pd.concat(results, axis=0)
 
-#fix types of the numerical columns:
+#fixing types of the numerical columns:
 results[['price', 'size', 'brs']] = results[['price', 'size', 'brs']].convert_objects(convert_numeric=True)
 
